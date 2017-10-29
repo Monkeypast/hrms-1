eeafc8f81446a801901923af2b8f5504985aec22
Myo Wai Yan Kyaw <myowaiyankyaw@hotmail.com>
2017-10-30 04:05:51 +0800

    Add Staff Details

--- hrms/reviews/tables.py
+++ hrms/reviews/tables.py
@@ -2,6 +2,7 @@ import django_tables2 as tables
 from django.contrib.auth.models import User
 
 class StaffTable(tables.Table):
+    Peer_Review = tables.TemplateColumn('<a href="/reviews/wine/5">Review</a>')
     class Meta:
         model = User
         # add class="paleblue" to <table> tag
