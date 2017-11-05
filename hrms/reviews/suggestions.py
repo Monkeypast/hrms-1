108541d8158a53584ba530ad4fa7f7a61f6ee436
Myo Wai Yan Kyaw <myowaiyankyaw@hotmail.com>
2017-11-06 03:07:25 +0800

    V.1.2

--- hrms/reviews/suggestions.py
+++ hrms/reviews/suggestions.py
@@ -41,6 +41,22 @@ def update_clusters():
         for i,cluster_label in enumerate(clustering.labels_):
             new_clusters[cluster_label].users.add(User.objects.get(username=all_user_names[i]))
             
+
+def save_resignee_from_row(resignee_row):
+    resignee = EmpPossibleResigneeReview()
+#     print(resignee_row['satisfaction'])
+    resignee.id = resignee_row['index']
+    resignee.satisfaction = resignee_row['satisfaction']
+    resignee.evaluation = resignee_row['evaluation']
+    resignee.projectCount = resignee_row['projectCount']
+    resignee.averageMonthlyHours = resignee_row['averageMonthlyHours']
+    resignee.yearsAtCompany = resignee_row['yearsAtCompany']
+    resignee.workAccident = resignee_row['workAccident']
+    resignee.promotion = resignee_row['promotion']
+    resignee.department = resignee_row['department']
+    resignee.salary = resignee_row['salary']
+    resignee.turnover = resignee_row['turnover']
+    resignee.save()
             
 def train_Algorithm():
     #engine = create_engine('sqlite:///../db.sqlite3')
@@ -63,11 +79,13 @@ def train_Algorithm():
     
     # Convert these variables into categorical variables
     #df["department"] = df["department"].astype('category').cat.codes
+    #df["salary"] = df["salary"].astype('category').cat.codes
     df['department'].replace(['sales', 'accounting', 'hr', 'technical', 'support', 'management',
         'IT', 'product_mng', 'marketing', 'RandD'], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], inplace = True)
-    df['salary'].replace(['low', 'medium', 'high'], [0, 1, 2], inplace = True)
-    #df["salary"] = df["salary"].astype('category').cat.codes
-    print(df)
+    df['salary'].replace(['LOW', 'MEDIUM', 'HIGH'], [0, 1, 2], inplace = True)
+    df['turnover'].replace(['NO', 'YES'], [0, 1], inplace = True)
+    df['workAccident'].replace(['NO', 'YES'], [0, 1], inplace = True)
+    df['promotion'].replace(['NO', 'YES'], [0, 1], inplace = True)
     
     # Move the reponse variable "turnover" to the front of the table
     front = df['turnover']
@@ -96,5 +114,35 @@ def train_Algorithm():
     
     data_out = X_current[X_current.turnover > 0]
     data_out = data_out.drop('int', axis=1)
+
+    EmpPossibleResigneeReview.objects.all().delete()
+#     for resignee in data_out.count(axis=0)
+#         reg = EmpPossibleResigneeReview()
+#         reg.id = resignee[0]
+#         reg.satisfaction = resignee[1]
+#         reg.evaluation = resignee[2]
+#         reg.projectCount = resignee[3]
+#         reg.averageMonthlyHours = resignee[4]
+#         reg.yearsAtCompany = resignee[5]
+#         reg.workAccident = resignee[6]
+#         reg.promotion = resignee[7]
+#         reg.department = resignee[8]
+#         reg.salary = resignee[9]
+#         reg.turnover = resignee[10]
+#         reg.save()
+        
+#     new_possible_resignee = {i: EmpPossibleResigneeReview(name=i) for i in range(k)}
+#     for cluster in new_possible_resignee.values(): # clusters need to be saved before refering to users
+#         cluster.save()
+#     for i,cluster_label in enumerate(clustering.labels_):
+#         new_clusters[cluster_label].users.add(User.objects.get(username=all_user_names[i]))
+    #data_out.index.name = 'id'
+    data_out.reset_index(level=0, inplace=True)
+    #data_out['id'] = data_out.index
+    print(data_out)
+    data_out.apply(
+        save_resignee_from_row,
+        axis=1
+    )
     #data_out.to_sql('reviews_emppossibleresigneereview',engine, if_exists='replace', index=True, index_label='id')
     
